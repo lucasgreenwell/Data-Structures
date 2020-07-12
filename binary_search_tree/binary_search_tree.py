b078c30f3b645ae1189d1d9191ebf4e6a9ea1491
Lucas Greenwell <lucasgreenwell15@gmail.com>
2020-07-11 22:46:29 -0600

    finishes in_order_print. also there's a weird bug with foreach that I need to fix

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -57,7 +57,7 @@ class BinarySearchTree:
         cb(self.value)
         if self.right:
             self.right.for_each(cb)
-        elif self.left:
+        if self.left:
             self.left.for_each(cb)
 
 
@@ -66,7 +66,12 @@ class BinarySearchTree:
     # Print all the values in order from low to high
     # Hint:  Use a recursive, depth first traversal
     def in_order_print(self, node):
-        pass
+        if node.left:
+            node.left.in_order_print(node.left)
+        print(node.value)
+        if node.right:
+            node.right.in_order_print(node.right)
+
 
     # Print the value of every node, starting with the given node,
     # in an iterative breadth first traversal
