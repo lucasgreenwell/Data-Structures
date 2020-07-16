09a9c847115ad73778f1acd094bf90ae491e9dbf
Lucas Greenwell <lucasgreenwell15@gmail.com>
2020-07-15 21:34:18 -0600

    works on heaps a bit, but doesn't get very far

--- heap/generic_heap.py
+++ heap/generic_heap.py
@@ -7,11 +7,11 @@ class Heap:
     def insert(self, value):
         self.storage.append(value)
         self.size += 1
-        self._bubble_up(self.get_size())
+        self._bubble_up(self.get_size() - 1)
 
     def delete(self):
         self.storage.pop()
-        self._bubble_up(self.get_size)
+        # self._bubble_up(self.get_size() - 1)
         return self.get_size()
 
     def get_priority(self):
@@ -19,17 +19,12 @@ class Heap:
 
     def get_size(self):
         return len(self.storage)
-
-    def _bubble_up(self, index):
-        while index > 0:
-            # compare to parent
-            parent = (index - 1) // 2
-
-            if self.storage[index] > self.storage[parent]:
-                self.storage[index], self.storage[parent] = self.storage[parent], self.storage[index]
-                index = parent
-            else:
-                break
+# not working still
+    def _bubble_up(self, moving_index):
+        parent_index = (moving_index - 1)//2
+        if self.storage[moving_index] > self.storage[parent_index]:
+            self.storage[parent_index] = self.storage[moving_index]
+            self.storage[moving_index] = self.storage[parent_index]
 
     def _sift_down(self, index):
         pass
--- heap/max_heap.py
+++ heap/max_heap.py
@@ -5,7 +5,7 @@ class Heap:
 
     def insert(self, value):
         self.storage.append(value)
-        self._bubble_up(self.get_size())
+        self._bubble_up(self.get_size() - 1)
 
     def delete(self):
         pass
@@ -17,12 +17,14 @@ class Heap:
         return len(self.storage)
 
     def _bubble_up(self, index):
-        children = self.storage[index]
-        parentIndex = math.floor((index - 1) / 2) + 1
+        if index is 0:
+            return
+        child = self.storage[index]
+        parentIndex = math.floor((index - 1) / 2)
         parent = self.storage[parentIndex]
-        if children > parent:
+        if child > parent and child is not self.storage[0]:
             self.storage[index] = parent
-            self.storage[parentIndex] = children
+            self.storage[parentIndex] = child
             self._bubble_up(parentIndex)
 
     def _sift_down(self, index):
